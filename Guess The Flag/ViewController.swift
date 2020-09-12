efc2bf8575c2f094984925704551543976cc1131
JeffMorhous <jobboy18@gmail.com>
2020-09-12 16:36:27 -0400

    Initial setup

--- Guess The Flag/ViewController.swift
+++ Guess The Flag/ViewController.swift
@@ -10,9 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fillCountries()
+        askQuestion()
+        customizeButtons()
+    }
+    
+    func fillCountries(){
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    }
+    
+    func askQuestion() {
+        countries.shuffle() //Randomize!
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+    
+    func customizeButtons() {
+        button1.layer.borderWidth = 1
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderWidth = 1
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderWidth = 1
+        button3.layer.borderColor = UIColor.lightGray.cgColor
     }
 
 
