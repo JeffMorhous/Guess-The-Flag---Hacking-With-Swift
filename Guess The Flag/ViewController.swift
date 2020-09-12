7440190c36b7fbc923dee6c951bb0ecb4a8f5012
JeffMorhous <jobboy18@gmail.com>
2020-09-12 16:54:33 -0400

    Wrap up the game!

--- Guess The Flag/ViewController.swift
+++ Guess The Flag/ViewController.swift
@@ -16,11 +16,12 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         fillCountries()
-        askQuestion()
+        askQuestion(action: nil)
         customizeButtons()
     }
     
@@ -28,8 +29,10 @@ class ViewController: UIViewController {
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction!) {
         countries.shuffle() //Randomize!
+        correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
@@ -44,6 +47,19 @@ class ViewController: UIViewController {
         button3.layer.borderColor = UIColor.lightGray.cgColor
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+    }
+    
 }
 
